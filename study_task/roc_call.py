6deec3b0df69dcbafbc8b7312c6010faa19f4e9f
Josh Koen <jkoen@nd.edu>
2020-10-31 21:40:53 -0400

    update template

--- study_task/roc_call.py
+++ study_task/roc_call.py
@@ -1,10 +1,9 @@
-import matlab.engine
 import matlab
+import numpy as np
+import matplotlib.pyplot as plt
 
 eng = matlab.engine.start_matlab()
 eng.addpath(eng.genpath('/opt/matlab_software/roc_toolbox'), nargout=0)
-
-x0, lb, ub = eng.gen_pars('dpsd',6,1,['Ro','F'])
 
 targf = [96,15,16,17,17,21,5,9,2,2]
 luref = [9,13,23,29,22,26,29,25,9,15]
@@ -21,3 +20,10 @@ eng.workspace['targf'] = matlab.double(targf)
 eng.workspace['luref'] = matlab.double(luref)
 roc_data = eng.eval("roc_solver(targf,luref,model,fitStat,x0,lb,ub,'figure',false)", nargout=1)
 
+Ro = roc_data['dpsd_model']['parameters']['Ro']
+F = roc_data['dpsd_model']['parameters']['F']
+criterion = np.asarray(roc_data['dpsd_model']['parameters']['criterion'])
+targ_proportion = np.asarray(roc_data['observed_data']['target']['cumulative'])
+lure_proportion = np.asarray(roc_data['observed_data']['lure']['cumulative'])
+
+eng.quit()
