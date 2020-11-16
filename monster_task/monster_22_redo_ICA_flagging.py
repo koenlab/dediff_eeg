b97928d677955f713a14583b5136d401dc165532
Josh Koen <jkoen@nd.edu>
2020-11-16 08:51:08 -0500

    reset redo_ICA_flagging code

--- monster_task/monster_22_redo_ICA_flagging.py
+++ monster_task/monster_22_redo_ICA_flagging.py
@@ -23,50 +23,30 @@ for sub_string in sub_list:
     print(f'Preprocessing task-{task} data for {sub_string}')
     print(f'  Derivatives Folder: {deriv_path}')
 
-    # # Load ICA Epochs
-    # epochs_fif_file = deriv_path / f'{sub_string}_task-{task}_ref-FCz_desc-ica_epo.fif.gz'
-    # epochs = read_epochs(epochs_fif_file)
+    # Load ICA Epochs
+    epochs_fif_file = deriv_path / f'{sub_string}_task-{task}_ref-FCz_desc-ica_epo.fif.gz'
+    epochs = read_epochs(epochs_fif_file)
 
-    # # Load ICA
-    # ica_file = deriv_path / f'{sub_string}_task-{task}_ref-FCz_desc-ica_ica.fif.gz'
-    # ica = read_ica(ica_file)
+    # Load ICA
+    ica_file = deriv_path / f'{sub_string}_task-{task}_ref-FCz_desc-ica_ica.fif.gz'
+    ica = read_ica(ica_file)
 
-    # # Plot ICA
-    # ica.plot_components(inst=epochs, reject=None,
-    #                     psd_args=dict(fmax=70))
-    # ica.save(ica_file)
+    # Plot ICA
+    ica.plot_components(inst=epochs, reject=None,
+                        psd_args=dict(fmax=70))
+    ica.save(ica_file)
 
-    # # Load json file for updating
-    # json_file = deriv_path / f'{sub_string}_task-{task}_ref-FCz_desc-ica_ica.json'
-    # with open(json_file, 'r') as f:
-    #     json_info = json.load(f)
+    # Load json file for updating
+    json_file = deriv_path / f'{sub_string}_task-{task}_ref-FCz_desc-ica_ica.json'
+    with open(json_file, 'r') as f:
+        json_info = json.load(f)
 
-    # # Only update description, proportion of flagged components, and glaffed components
-    # json_info['Description'] = 'ICA components'
-    # json_info['flagged_components'] = [int(x) for x in ica.exclude]
-    # json_info['proportion_components_flagged'] = len(ica.exclude)/len(ica.info['ch_names'])
+    # Only update description, proportion of flagged components, and glaffed components
+    json_info['Description'] = 'ICA components'
+    json_info['flagged_components'] = [int(x) for x in ica.exclude]
+    json_info['proportion_components_flagged'] = len(ica.exclude)/len(ica.info['ch_names'])
     
-    # # Resave json file
-    # with open(json_file, 'w') as f:
-    #     json.dump(json_info, f, indent=4)
+    # Resave json file
+    with open(json_file, 'w') as f:
+        json.dump(json_info, f, indent=4)
     
-    files = ['ica', 'removedICs', 'cleaned']
-    for x in files:
-        if x == 'ica':
-            tmin, tmax = -1.0, 1.0
-            ref = 'FCz'
-        else:
-            tmin, tmax = -1.7, 1.7
-            ref = 'avg'
-        json_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_desc-{x}_epo.json'
-        if not json_file.is_file():
-            continue
-        with open(json_file, 'r') as f:
-            json_info = json.load(f)
-        
-        json_info['tmin'] = tmin
-        json_info['tmax'] = tmax
-        if x == 'cleaned':
-            json_info['metadata'] = (deriv_path / f'{sub_string}_task-{task}_desc-cleaned_metadata.tsv').name
-        with open(json_file, 'w') as f:
-            json.dump(json_info, f, indent=4)
