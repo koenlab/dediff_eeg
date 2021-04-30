d0c5fe2ba2e4fa6e107e40aabf5db79be77f04fc
Josh Koen <jkoen@nd.edu>
2021-04-30 15:59:22 -0400

    Update study TF analyses

--- preprocessing/study_task/study_05_compute_tf_hilbert.py
+++ preprocessing/study_task/study_05_compute_tf_hilbert.py
@@ -23,11 +23,11 @@ ref = 'avg'
 
 #####---Define Frequency Bands---#####
 freq_bands = OrderedDict(
-    theta = dict(l_freq=4, h_freq=7, l_trans_bandwidth=1, h_trans_bandwidth=1),
-    alpha = dict(l_freq=8, h_freq=12, l_trans_bandwidth=1, h_trans_bandwidth=1),
-    lowbeta = dict(l_freq=13, h_freq=17, l_trans_bandwidth=1, h_trans_bandwidth=1),
-    highbeta = dict(l_freq=18, h_freq=25, l_trans_bandwidth=1, h_trans_bandwidth=1),
-    gamma = dict(l_freq=30,h_freq=50, l_trans_bandwidth=1, h_trans_bandwidth=1)
+    theta = dict(l_freq=4, h_freq=7, l_trans_bandwidth=2, h_trans_bandwidth=2),
+    alpha = dict(l_freq=8, h_freq=12, l_trans_bandwidth=2, h_trans_bandwidth=2),
+    lowbeta = dict(l_freq=13, h_freq=17, l_trans_bandwidth=2, h_trans_bandwidth=2),
+    highbeta = dict(l_freq=18, h_freq=25, l_trans_bandwidth=2, h_trans_bandwidth=2),
+    gamma = dict(l_freq=30,h_freq=50, l_trans_bandwidth=2, h_trans_bandwidth=2)
 )
 
 scenes = []
@@ -43,16 +43,16 @@ for sub_string in sub_list:
     deriv_path = deriv_dir / sub_string
     print(f'Creating TF data (Hilbert Transform) for task-{task} data for {sub_string}')
     print(f'  Derivatives Folder: {deriv_path}')
-    
+
     # Load epochs
     epochs_fif_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_desc-cleaned_epo.fif.gz'
     epochs = read_epochs(epochs_fif_file)
-    
-    # Power output array
+
+    # amp output array
     n_trials, n_chans, n_times = epochs._data.shape
     n_freqs = len(freq_bands.keys())
     tf_shape = (n_trials, n_chans, n_freqs, n_times)
-    power = np.zeros(tf_shape)
+    amp = np.zeros(tf_shape)
     phase = np.zeros(tf_shape)
     center_f = np.zeros(n_freqs)
     
@@ -71,89 +71,66 @@ for sub_string in sub_list:
         epochs_hilb = epochs_band.copy()
         epochs_hilb.apply_hilbert(picks=['eeg'], n_jobs=4)
         
-        # Compute power and phase
-        power[:,:,i,:] = np.abs(epochs_hilb._data.real)**2
+        # Compute amp and phase
+        amp[:,:,i,:] = np.abs(epochs_hilb._data.real)
         phase[:,:,i,:] = np.angle(epochs_hilb._data)
         
         # Compute center frequency
         center_f[i] = np.mean([opts['l_freq'], opts['h_freq']])
 
     # Make an EpochsTFR using center frequency as the point in each band
-    power_epochs = EpochsTFR(epochs.info, power, epochs.times, center_f,
+    amp_epochs = EpochsTFR(epochs.info, amp, epochs.times, center_f,
                         method='hilbert', 
                         events=epochs.events,
                         event_id=epochs.event_id,
                         metadata=epochs.metadata,
                         verbose=True)
+    amp_epochs.crop(tmin=-.5, tmax=1.5)
 
-    
-    # Save the power
-    power_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-power_tfr.h5'
-    # power_epochs.save(power_file, overwrite=True)
-    
-    # # Make JSON
-    # json_info = {
-    #     'Description': 'TFR Power from Hilbert Transform',
-    #     'baseline': dict(twin='n/a', mode='n/a', sort_keys=True), 
-    #     'sfreq': power_epochs.info['sfreq'],
-    #     'reference': 'average',
-    #     'bandpass_filters': dict(freq_bands, sort_keys=True),
-    #     'freq_centers': list(center_f),
-    #     'tmin': power_epochs.times.min(),
-    #     'tmax': power_epochs.times.max()
-    # }
-    # json_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-power_tfr.json'
-    # with open(json_file, 'w') as outfile:
-    #     json.dump(json_info, outfile, indent=4)
-    
-    
-    # # Make an EpochsTFR using center frequency as the point in each band
-    # phase_epochs = EpochsTFR(epochs.info, phase, epochs.times, center_f,
-    #                   method='hilbert', 
-    #                   events=epochs.events,
-    #                   event_id=epochs.event_id,
-    #                   metadata=epochs.metadata,
-    #                   verbose=True)
-    
-    # # Save the phase
-    # phase_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-phase_tfr.h5'
-    # phase_epochs.save(phase_file, overwrite=True)
-    
-    # # Make JSON
-    # json_info = {
-    #     'Description': 'TFR Phase from Hilbert Transform',
-    #     'baseline': dict(twin='n/a', mode='n/a', sort_keys=True), 
-    #     'sfreq': phase_epochs.info['sfreq'],
-    #     'reference': 'average',
-    #     'bandpass_filters': dict(freq_bands, sort_keys=True),
-    #     'freq_centers': list(center_f),
-    #     'tmin': phase_epochs.times.min(),
-    #     'tmax': phase_epochs.times.max()
-    # }
-    # json_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-phase_tfr.json'
-    # with open(json_file, 'w') as outfile:
-    #     json.dump(json_info, outfile, indent=4)
-    
-    tfr = power_epochs.copy()
-    scene = tfr["category=='scene' and study_n_responses==1 and test_resp in [5,6]"].average()
-    scenes.append(scene.apply_baseline((-.4, -.1), mode='percent'))
-    scene.plot_topo(picks=['eeg'], baseline=(-.5,-.2), mode='logratio')
+    # Save the amp
+    amp_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-amp_tfr.h5'
+    amp_epochs.save(amp_file, overwrite=True)
 
+    # Make JSON
+    json_info = {
+        'Description': 'TFR amplitude from Hilbert Transform',
+        'baseline': dict(twin='n/a', mode='n/a', sort_keys=True), 
+        'sfreq': amp_epochs.info['sfreq'],
+        'reference': 'average',
+        'bandpass_filters': dict(freq_bands, sort_keys=True),
+        'freq_centers': list(center_f),
+        'tmin': amp_epochs.times.min(),
+        'tmax': amp_epochs.times.max()
+    }
+    json_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-amp_tfr.json'
+    with open(json_file, 'w') as outfile:
+        json.dump(json_info, outfile, indent=4)
     
-    obj = tfr["category=='scene' and study_n_responses==1 and test_resp in [1,2,3,4]"].average()
-    objects.append(obj.apply_baseline((-.4, -.1), mode='percent'))
-    obj.plot_topo(picks=['eeg'], baseline=(-.5,-.2), mode='logratio')
     
-    evokeds = [scene,objects]
-    smo = mne.combine_evoked(evokeds, weights=[1, -1])
-    smos.append(smo)
-    smo.plot_topo(picks=['eeg'], tmin=-.5, tmax=1.5, fmax=25)
-    smo.plot_topomap(tmin=.3,tmax=.5, fmin=5.5, fmax=5.5, ch_type='eeg',
-                       baseline=(-.4,-.1), mode='logratio')
+    # Make an EpochsTFR using center frequency as the point in each band
+    phase_epochs = EpochsTFR(epochs.info, phase, epochs.times, center_f,
+                      method='hilbert', 
+                      events=epochs.events,
+                      event_id=epochs.event_id,
+                      metadata=epochs.metadata,
+                      verbose=True)
+    phase_epochs.crop(tmin=-.5, tmax=1.5)
     
-# scene_grand = mne.grand_average(scenes)
-# scene_grand.plot_topo(picks=['eeg'])
-# obj_grand = mne.grand_average(objects)
-# obj_grand.plot_topo(picks=['eeg'])
-# smo_grand = mne.grand_average(smos)
-# smo.plot_topo(picks=['eeg'])
+    # Save the phase
+    phase_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-phase_tfr.h5'
+    phase_epochs.save(phase_file, overwrite=True)
+    
+    # Make JSON
+    json_info = {
+        'Description': 'TFR Phase from Hilbert Transform',
+        'baseline': dict(twin='n/a', mode='n/a', sort_keys=True), 
+        'sfreq': phase_epochs.info['sfreq'],
+        'reference': 'average',
+        'bandpass_filters': dict(freq_bands, sort_keys=True),
+        'freq_centers': list(center_f),
+        'tmin': phase_epochs.times.min(),
+        'tmax': phase_epochs.times.max()
+    }
+    json_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_mode-hilbert_value-phase_tfr.json'
+    with open(json_file, 'w') as outfile:
+        json.dump(json_info, outfile, indent=4)
--- preprocessing/study_task/study_06_compute_tf_morlet.py
+++ preprocessing/study_task/study_06_compute_tf_morlet.py
@@ -7,11 +7,13 @@ evoked objects for conditions of interest.
 
 #####---Import Libraries---#####
 from sys import modules
+
 import numpy as np
 import json
+from collections import OrderedDict
 
 from mne import read_epochs
-from mne.time_frequency import tfr_morlet
+from mne.time_frequency import (tfr_morlet, tfr_array_morlet, EpochsTFR)
 import mne
 
 from study_config import (bids_dir, deriv_dir, task, preprocess_options, get_sub_list)
@@ -25,7 +27,26 @@ ref = 'avg'
 
 # Frequecies to estimate
 freqs = np.arange(3,51,1) # Estimate 4-50Hz in linear intervals
-n_cycles = 5 # of cycles
+n_cycles = 6 # of cycles
+
+# Conditions and queries to look at
+queries = {
+        'all': "study_n_responses==1 and test_resp in [1,2,3,4,5,6]",
+        'scene': "category=='scene' and study_n_responses==1 and test_resp in [1,2,3,4,5,6]",
+        'object': "category=='object' and study_n_responses==1 and test_resp in [1,2,3,4,5,6]",
+        'hit65': "study_n_responses==1 and test_resp in [5,6]",
+        'miss65': "study_n_responses==1 and test_resp in [1,2,3,4]",
+        'scenehit65': "category=='scene' and study_n_responses==1 and test_resp in [5,6]",
+        'scenemiss65': "category=='scene' and study_n_responses==1 and test_resp in [1,2,3,4]",
+        'objecthit65': "category=='object' and study_n_responses==1 and test_resp in [5,6]",
+        'objectmiss65': "category=='object' and study_n_responses==1 and test_resp in [1,2,3,4]",
+        'hit6': "study_n_responses==1 and test_resp in [6]",
+        'miss6': "study_n_responses==1 and test_resp in [1,2,3,4,5]",
+        'scenehit6': "category=='scene' and study_n_responses==1 and test_resp in [6]",
+        'scenemiss6': "category=='scene' and study_n_responses==1 and test_resp in [1,2,3,4,5]",
+        'objecthit6': "category=='object' and study_n_responses==1 and test_resp in [6]",
+        'objectmiss6': "category=='object' and study_n_responses==1 and test_resp in [1,2,3,4,5]"
+    }
 
 # Ask for subject IDs to analyze
 sub_list = get_sub_list(deriv_dir, allow_all=True)
@@ -36,51 +57,53 @@ for sub_string in sub_list:
     deriv_path = deriv_dir / sub_string
     print(f'Creating TF data (Morlet) for task-{task} data for {sub_string}')
     print(f'  Derivatives Folder: {deriv_path}')
-    
+
     # Load epochs
     epochs_fif_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_desc-cleaned_epo.fif.gz'
     epochs = read_epochs(epochs_fif_file)
-    
+
     # Estiamte TFR
-    tfr = tfr_morlet(epochs, freqs=freqs, n_cycles=n_cycles, use_fft=False,
-                     return_itc=False, picks=['eeg'], average=False,
-                     output='power', n_jobs=1, verbose=True)
-    
-    # Save the power
-    tfr_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_method-morlet_value-power_tfr.h5'
-    tfr.save(tfr_file, overwrite=True)
-    
-    # Make JSON
-    json_info = {
-        'Description': 'TFR Power from Morlet Wavelets',
-        'baseline': dict(twin='n/a', mode='n/a', sort_keys=True), 
-        'sfreq': tfr.info['sfreq'],
-        'reference': 'average',
-        'tmin': tfr.times.min(),
-        'tmax': tfr.times.max(),
-        'freqs': freqs.tolist(),
-        'n_cycles': n_cycles
-    }
-    json_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_method-morlet_value-power_tfr.json'
-    with open(json_file, 'w') as outfile:
-        json.dump(json_info, outfile, indent=4)
+    for comment, query in queries.items():
         
-# #     # # 
-#     scene = tfr["category=='scene' and study_n_responses==1 and test_resp in [5,6]"].average()
-#     scenes.append(scene.apply_baseline((-.4, -.1), mode='logratio'))
-    
-#     # scene.plot_topo(picks=['eeg'], baseline=(-.5,.2), mode='logratio')
-#     obj = tfr["category=='scene' and study_n_responses==1 and test_resp in [1,2,3,4]"].average()
-#     objects.append(obj.apply_baseline((-.4, -.1), mode='logratio'))
-#     # object.plot_topo(picks=['eeg'], baseline=(-.5,.2), mode='logratio')
-#     evokeds = [scene,obj]
-#     smo = mne.combine_evoked(evokeds, weights=[1, -1])
-#     smos.append(smo)
-#     smo.plot_topo(picks=['eeg'], tmin=-.5, tmax=1.5, fmax=25)
-    
-# # scene_grand = mne.grand_average(scenes)
-# # scene_grand.plot_topo(picks=['eeg'])
-# # obj_grand = mne.grand_average(objects)
-# # obj_grand.plot_topo(picks=['eeg'])
-# # smo_grand = mne.grand_average(smos)
-# # smo_grand.plot_topo(picks=['eeg'])
+        # Get the data and pad it
+        tmp_epochs = epochs[query]
+        epoch_data = tmp_epochs.get_data()
+        pad_before, pad_after = np.array([1, 1]) / (1/epochs.info['sfreq'])
+        pad_width = ((0,0),(0,0),(int(pad_before), int(pad_after)))
+        epoch_data = np.pad(epoch_data, pad_width, mode='symmetric')
+        
+        # Compute power (averaged)
+        tfr_data = tfr_array_morlet(epoch_data, epochs.info['sfreq'], freqs=freqs, 
+                                    n_cycles=n_cycles, use_fft=True, output='power', 
+                                    n_jobs=1)
+        tfr_data = tfr_data[:,:,:,int(pad_before):]
+        tfr_data = tfr_data[:,:,:,:-int(pad_before)]
+        power = EpochsTFR(epochs.info, tfr_data, epochs.times, freqs,
+                            method='morlet', 
+                            events=epochs.events,
+                            event_id=epochs.event_id,
+                            metadata=epochs.metadata,
+                            verbose=True).average()
+
+        # Crop and apply baseline to TFR
+        power.crop(tmin=-.5, tmax=1.5).apply_baseline((-.5,-.2), mode='logratio')
+        power.comment = comment
+
+        # Save the power
+        tfr_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_method-morlet_cond-{comment}_tfr.h5'
+        power.save(tfr_file, overwrite=True)
+
+        # Make JSON
+        json_info = {
+            'Description': 'TFR Power from Morlet Wavelets',
+            'baseline': dict(twin=(-.5,-.2), mode='logratio', sort_keys=True), 
+            'sfreq': power.info['sfreq'],
+            'reference': 'average',
+            'tmin': power.times.min(),
+            'tmax': power.times.max(),
+            'freqs': freqs.tolist(),
+            'n_cycles': n_cycles
+        }
+        json_file = deriv_path / f'{sub_string}_task-{task}_ref-{ref}_method-morlet_cond-{comment}_tfr.json'
+        with open(json_file, 'w') as outfile:
+            json.dump(json_info, outfile, indent=4)
