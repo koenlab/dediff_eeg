42c5d8aad86592d37d6d025b399b2decbabd559f
Josh Koen <koen.joshua@gmail.com>
2021-08-16 14:48:39 -0400

    code reorg

--- monster_task/extra.py
+++
@@ -1,7 +0,0 @@
-epochs_ar = epochs_ar['stan']
-top = epochs_ar['gabor_loc=="top" and correct==1'].copy().crop(tmin=-.2, tmax=.6).average()
-bot = epochs_ar['bot'].copy().crop(tmin=-.2, tmax=.6).average()
-
-evokeds = [top.filter(None,20), bot.filter(None,20)]
-diff = mne.combine_evoked(evokeds, weights=[1, -1])
-mne.viz.plot_compare_evokeds(evokeds, axes='topo')
